ccc76bc84912e5449609c2ac50bc7cd3dee26fe4
libaiyu_1 <709097185@qq.com>
2017-02-10 15:58:22 +0800

    execl->excel. add attention 3 times. add count total files that have been cleared.

--- clearExcel.py
+++ clearExcel.py
@@ -1,8 +1,12 @@
 # _*_ coding: utf_8  _*_
+# Attention: this will damage all xlsx files' data
  
 import openpyxl
 import os
 import re
+
+for k in range(3):
+    input("Attention  %d : this will damage all xlsx files' data. Are you sure." % (k + 1))
 
 # find the file that include '学生名单' in filename
 ChineseReg = re.compile(r'学生名单')
@@ -11,9 +15,11 @@ excelReg = re.compile(r'.xlsx')
 wb = openpyxl.Workbook()
 sheet = wb.get_active_sheet()
 
-
-for fileName in os.listdir('d:\\_PythonWorks\\execlOperate\\pscj161702'):
+count = 0
+for fileName in os.listdir('d:\\_PythonWorks\\excelOperate\\pscj161702'):
     if excelReg.search(fileName):
-#        if ChineseReg.search(fileName) == None:
+        if ChineseReg.search(fileName) == None:
             wb.save(fileName)
-            print('file %s is clear!',fileName)
+            count += 1
+            print('file %s   is clear!' % (fileName))
+print('total %d files is clear!' % (count))
