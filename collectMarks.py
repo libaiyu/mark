9430a3861449d04c72d146f2ed2e22cb237c0525
evan <909798432@qq.com>
2017-02-11 23:30:44 +0800

    modify loop

--- collectMarks.py
+++ collectMarks.py
@@ -43,38 +43,38 @@ dirname = 'd:\\_PythonWorks\\excelOperate\\cj-2016201701'
 for file in os.listdir(dirname):
     logging.debug(file)
     classSearch = classReg.search(file) # a = re.findall(classReg,file)
-    if classSearch:
-        logging.debug(classSearch.group())
-        logging.debug(className)
-        if classSearch.group() == className:
-            logging.debug(file )
-            fullname = dirname + '\\' + file
-            wb = openpyxl.load_workbook( fullname)
-            sheet = wb.get_active_sheet()
-            for twoDigit in range(33):                   ####
-                studentNum = str(int(studentNo) + twoDigit)    #####
-                for row in range(1,130):                  #####
-                    logging.info('row is:%d',row)
-                    logging.info( 'error test' + str( sheet['B'+str(row)].value ) )
-                    if( sheet['B'+str(row)].value == int(studentNum) ):               #  学号在B列
-                        logging.debug( ' '*2+str(row)+'  '+sheet['D'+str(row)].value )       #  姓名在D列
-                        logging.debug(' 课堂平时成绩：'+ str( sheet['J'+str(row)].value ) )  #  课堂平时成绩在J列
-                        logging.debug(' 课堂期末成绩：'+ str( sheet['M'+str(row)].value ) )  #  课堂期末成绩在M列
-                        logging.debug(' 课堂总成绩：'+ str( sheet['O'+str(row)].value ) )    #  课堂总成绩在O列
-                        logging.debug(' 实践成绩：'+ str( sheet['Q'+str(row)].value ) )      #  实践成绩在Q列
-                        logging.debug(' 实验成绩：'+ str( sheet['R'+str(row)].value ) )      #  实验成绩在R列
-                        logging.debug(' 总成绩：'+ str( sheet['S'+str(row)].value ) )        #  总成绩在S列
-                        studentMarks[0].append(file)
-                        studentMarks[1].append(studentNum)
-                        studentMarks[2].append(sheet['D'+str(row)].value)
-                        studentMarks[3].append(sheet['J'+str(row)].value)
-                        studentMarks[4].append(sheet['M'+str(row)].value)
-                        studentMarks[5].append(sheet['O'+str(row)].value)
-                        studentMarks[6].append(sheet['Q'+str(row)].value)
-                        studentMarks[7].append(sheet['R'+str(row)].value)
-                        studentMarks[8].append(sheet['S'+str(row)].value)                  
-                        break
-                logging.error( 'error test')
+    if not classSearch or classSearch.group() != className:
+        logging.debug('Doesn\' match!')
+        continue
+    logging.debug(classSearch.group())
+    logging.debug(className)
+    fullname = dirname + '\\' + file
+    wb = openpyxl.load_workbook( fullname)
+    sheet = wb.get_active_sheet()
+    for twoDigit in range(33):                   ####
+        studentNum = str(int(studentNo) + twoDigit)    #####
+        for row in range(1,130):                  #####
+            logging.info('row is:%d',row)
+            logging.info( 'error test' + str( sheet['B'+str(row)].value ) )
+            if( sheet['B'+str(row)].value == int(studentNum) ):               #  学号在B列
+                logging.debug( ' '*2+str(row)+'  '+sheet['D'+str(row)].value )       #  姓名在D列
+                logging.debug(' 课堂平时成绩：'+ str( sheet['J'+str(row)].value ) )  #  课堂平时成绩在J列
+                logging.debug(' 课堂期末成绩：'+ str( sheet['M'+str(row)].value ) )  #  课堂期末成绩在M列
+                logging.debug(' 课堂总成绩：'+ str( sheet['O'+str(row)].value ) )    #  课堂总成绩在O列
+                logging.debug(' 实践成绩：'+ str( sheet['Q'+str(row)].value ) )      #  实践成绩在Q列
+                logging.debug(' 实验成绩：'+ str( sheet['R'+str(row)].value ) )      #  实验成绩在R列
+                logging.debug(' 总成绩：'+ str( sheet['S'+str(row)].value ) )        #  总成绩在S列
+                studentMarks[0].append(file)
+                studentMarks[1].append(studentNum)
+                studentMarks[2].append(sheet['D'+str(row)].value)
+                studentMarks[3].append(sheet['J'+str(row)].value)
+                studentMarks[4].append(sheet['M'+str(row)].value)
+                studentMarks[5].append(sheet['O'+str(row)].value)
+                studentMarks[6].append(sheet['Q'+str(row)].value)
+                studentMarks[7].append(sheet['R'+str(row)].value)
+                studentMarks[8].append(sheet['S'+str(row)].value)                  
+                break
+        logging.error( 'error test')
 
 # Write marks    
 wb = openpyxl.Workbook()
