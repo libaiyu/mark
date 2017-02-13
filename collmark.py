d8cd9240a91e117573d23179e6e178f13c257178
libaiyu_1 <709097185@qq.com>
2017-02-13 22:11:35 +0800

    tuple as list's element.

--- collmark.py
+++ collmark.py
@@ -16,18 +16,26 @@ class collectmarks():
     """
 
     def __init__(self):
-        self.classes = []
+        self.classlist = []
+#        self.classdict = {}
 
 ##    def set_regular(self, list):
 ##        self.regexList.append(re.compile(s))        
 
     def classfind(self, dirname, relist):
         for file in os.listdir(dirname):
+            logging.info(file)
             CLASSREG = re.compile(relist)
-            self.classes.append(CLASSREG.search(file).group())
-        logging.info(self.classes)
+            if CLASSREG.search(file): 
+                self.classlist.append((CLASSREG.search(file).group(1),file))
+            #    self.classdict.setdefault(CLASSREG.search(file).group(1), file)
+                
+            #    list(self.classdict[CLASSREG.search(file).group()].value).append(file)
+        logging.info(self.classlist)
+        
+        # logging.info(self.classdict)
         input('any key to continue.')
-        return self.classes
+        return self.classlist
         
     def colclass(self, dirname, classname):
         pass
@@ -39,7 +47,7 @@ class collectmarks():
 def main():
     STUDENT_COUNT = 38
     DIRNAME = 'd:\\_PythonWorks\\excelOperate\\cj-2016201701'
-    RELIST = '\d{7}' # CLASSREG = re.compile(r'\d{7}[zZ]?')
+    RELIST = '-(\d{7}z?)\.' # CLASSREG = re.compile(r'\d{7}[zZ]?')
     COLUMNS_MAP = {
             '课堂平时成绩': {'column': 'J', 'index': 3},
             '课堂期末成绩': {'column': 'M', 'index': 4},
@@ -50,7 +58,7 @@ def main():
             }
     collmark = collectmarks()
     collmark.classfind(DIRNAME,RELIST)
-##    for classname in collmark.classes(DIRNAME):
+##    for classname in collmark.classfind(DIRNAME,RELIST):
 ##        collmark.collclass(classname)
 #    collmark.collstud(studnumb)
 
