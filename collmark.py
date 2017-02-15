d8bd5869f68b4710cccd92019a3742b9b4e8377c
libaiyu_1 <709097185@qq.com>
2017-02-15 22:26:31 +0800

    output is not good.

--- collmark.py
+++ collmark.py
@@ -15,8 +15,10 @@ class collectmarks():
 
     def __init__(self):
         self.classlist = []
+        self.allrowmark = []
+        self.studentsmarks = {}
         self.coursemark = []
-        self.allcoursemark = {}
+        self.classmark = {}
         self.studentmarks = []
 
     def classfind(self, srcdir, relist):
@@ -29,7 +31,8 @@ class collectmarks():
         return self.classlist
 
     def copymark(self, srcdir):
-        self.allcoursemark = {}    # for store marks of all courses, class as key, marks as the values    
+        self.classmark = {}    # for store marks of all courses, class as key, marks as the values
+        self.allrowmark = []   # for store all row marks
         for t in self.classlist:
             self.coursemark = []   # for store marks of one course
             classname = t[0]
@@ -41,24 +44,22 @@ class collectmarks():
             sheet = wb.get_active_sheet()
             for row in range(1,sheet.max_row):
                 # the first element of every row is file name(include course name)
-                rowmark = [file]      # for store marks of one row
+                rowmark = [file]               # for store marks of one row
                 for col in range(1,sheet.max_column):
-                    rowmark.append(sheet.cell(row = row, column = col).value)                
-                self.coursemark.append(rowmark)
+                    rowmark.append(sheet.cell(row = row, column = col).value)
+                self.allrowmark.append(rowmark)      # add one row marks to form all row marks
+                self.coursemark.append(rowmark)      # add one row marks to form a course marks
             #logging.info(self.coursemark)
 #            input('for debug 1')
-            self.allcoursemark.setdefault(classname, [])
-            self.allcoursemark[classname].append(self.coursemark)
-#            logging.info(self.allcoursemark)            
-#        logging.info(self.allcoursemark)   # data is too much to information                   
+            self.classmark.setdefault(classname, [])
+            self.classmark[classname].append(self.coursemark)
+#            logging.info(self.classmark)            
+#        logging.info(self.classmark)   # data is too much to information                   
 #        input('for debug 2')
-        pass
-    
-    def copyclass(self, srcdir, dstdir):
-        pass
-    
+
+    # copy one class' marks in one sheet. add the course name .     
     def writeclass(self, dstdir):        
-        for classname,mark in self.allcoursemark.items():
+        for classname,mark in self.classmark.items():
             wb = openpyxl.Workbook()            
             sheet = wb.get_active_sheet()
             # Write marks 
@@ -70,7 +71,7 @@ class collectmarks():
                 for r in range(len(mark[k])):
                     row += 1
                     for col in range(len(mark[k][r])):
-                        sheet.cell(row = row, column = col + 1).value = mark[k][r][col]
+                        sheet.cell(row = row, column = col + 2).value = mark[k][r][col]
                         logging.info(str(row) + ' ' + str(r) + ' ' + str(col) + ': ' + str(mark[k][r][col]))
             newfullname = dstdir + '\\cj-total-' + classname + '.xlsx'
             wb.save(newfullname)
@@ -80,7 +81,9 @@ class collectmarks():
         pass
     
     def writestudent(self, srcdir, dstdir):
+        
         pass
+    
     def copyall(self, srcdir, dstdir):
         pass
     def writeall(self, srcdir, dstdir):
@@ -111,67 +114,58 @@ class collectmarks():
         # Write marks    
         wb = openpyxl.Workbook()
         sheet = wb.get_active_sheet()
-        for row in range(1,len(self.studentmarks)):
-            for col in range(1,len(self.studentmarks[0])):
-                sheet.cell(row = row, column = col + 1).value = self.studentmarks[row - 1][col - 1]        
+        for row in range(1,len(self.studentmarks) + 1):
+            for col in range(1,len(self.studentmarks[0]) + 1):
+                sheet.cell(row = row, column = col + 1).value = self.studentmarks[row][col]        
         newfullname = dstdir + '\\cj-total' + '.xlsx'
-        wb.save(newfullname)
-
-    # copy one class' marks in one sheet. add the course name .    
-    def classmark(self, srcdir, dstdir):
-
-        pass
-
+        wb.save(newfullname)   
 
     # collectmarks for students one by one .
-    def tidymark(self):
-
-        studmark = [
-            ['课程',],
-            ['学号', ],
-            ['姓名', ],
-            ['课堂平时成绩',],
-            ['课堂期末成绩',],
-            ['课堂总成绩',],
-            ['实践成绩',],
-            ['实验成绩',],
-            ['总成绩',],
-            ]
-        COLUMNS_MAP = {
-                '课堂平时成绩': {'column': 'J', 'index': 3},
-                '课堂期末成绩': {'column': 'M', 'index': 4},
-                '课堂总成绩': {'column': 'O', 'index': 5},
-                '实践成绩': {'column': 'Q', 'index': 6},
-                '实验成绩': {'column': 'R', 'index': 7},
-                '总成绩': {'column': 'S', 'index': 8},
-                }
-        
-        for test in range(len(self.studentmarks[22])):
-            print(self.studentmarks[22][test])     #  column 2 (0,1,2) is student's number.
-        input('for debug')
-        
-        wb = openpyxl.Workbook()
-        sheet = wb.get_active_sheet()        
-
-        pass
+##    def tidymark(self):
+##
+##        studmark = [
+##            ['课程',],
+##            ['学号', ],
+##            ['姓名', ],
+##            ['课堂平时成绩',],
+##            ['课堂期末成绩',],
+##            ['课堂总成绩',],
+##            ['实践成绩',],
+##            ['实验成绩',],
+##            ['总成绩',],
+##            ]
+##        COLUMNS_MAP = {
+##                '课堂平时成绩': {'column': 'J', 'index': 3},
+##                '课堂期末成绩': {'column': 'M', 'index': 4},
+##                '课堂总成绩': {'column': 'O', 'index': 5},
+##                '实践成绩': {'column': 'Q', 'index': 6},
+##                '实验成绩': {'column': 'R', 'index': 7},
+##                '总成绩': {'column': 'S', 'index': 8},
+##                }
+##        
+##        for test in range(len(self.studentmarks[22])):
+##            print(self.studentmarks[22][test])     #  column 2 (0,1,2) is student's number.
+##        input('for debug')
+##        
+##        wb = openpyxl.Workbook()
+##        sheet = wb.get_active_sheet()        
+##
+##        pass
 
 
 def main():
     SRCDIR = 'd:\\_PythonWorks\\excelOperate\\cj-2016201701'
     DSTDIR = 'd:\\_PythonWorks\\excelOperate\\cjcl-2016201701'
-    RELIST = '-(\d{7}z?)\.' # CLASSREG = re.compile(r'\d{7}[zZ]?')
+    CLASSRE = '-(\d{7}z?)\.' # CLASSREG = re.compile(r'\d{7}[zZ]?')
+    STUDRE = '\d{12}z?'  # STUDREG = re.compile(r'\d{12}z?')
+    STUDNUM = input("please input student's number: ")
 
     collmark = collectmarks()
-    CLASSLIST = collmark.classfind(SRCDIR,RELIST)
-    
+    CLASSLIST = collmark.classfind(SRCDIR,RELIST)    
     collmark.copymark(SRCDIR)    
-
-#    collmark.copyclass(SRCDIR,DSTDIR)
     collmark.writeclass(DSTDIR)
 
-#    input('for debug')
-    
-##    collmark.writestudent(SRCDIR,DSTDIR)
+    collmark.writestudent(STUDNUM,DSTDIR)
 ##
 ##    collmark.copyall(SRCDIR,DSTDIR)
 ##    collmark.writeall(SRCDIR,DSTDIR)
