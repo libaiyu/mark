16e1402c099fd8db796c6f88669b6935bd352e77
libaiyu <709097185@qq.com>
2017-03-12 12:17:38 +0800

    add some tags 课堂作业,作业，报告。 Fix IndexError by Evan.

--- coursemark.py
+++ coursemark.py
@@ -9,6 +9,8 @@ Record can not be written. prompt is not good enough.   2017-2-11-10:50
 Student's number must be 2 digits. When it is smaller than 10, It should be 0X.  2017-2-11-17:23
 tidy the value. add display the 总分。display the rank.         2017-2-20
 debug the course regular.      2017-3-4.
+fix the IndexError.     by evan              2017-3-11
+add save error capture. add some Tags.       2017-3-12
 '''
 
 import openpyxl
@@ -38,6 +40,8 @@ performance_tag = [
     ['早退',],
     ['提出问题',],
     ['回答问题',],
+    ['课堂作业',],
+    ['作业1',],['作业2',],['作业3',],['作业4',],['作业5',],['作业6',],['作业7',],
     ]
 lab_tag = [
     ['旷课',],
@@ -45,6 +49,7 @@ lab_tag = [
     ['早退',],
     ['操作1',],['操作2',],['操作3',],['操作4',],['操作5',],['操作6',],['操作7',],['操作8',],
     ['数据1',],['数据2',],['数据3',],['数据4',],['数据5',],['数据6',],['数据7',],['数据8',],   
+    ['报告1',],['报告2',],['报告3',],['报告4',],
     ]
 design_tag = [
     ['旷课',],
@@ -52,6 +57,7 @@ design_tag = [
     ['早退',],
     ['设计1',],['设计2',],['设计3',],['设计4',],
     ['数据1',],['数据2',],['数据3',],['数据4',],  
+    ['报告1',],['报告2',],
     ]
 practice_tag = [
     ['旷课',],
@@ -59,6 +65,7 @@ practice_tag = [
     ['早退',],
     ['操作1',],['操作2',],['操作3',],['操作4',],
     ['数据1',],['数据2',],['数据3',],['数据4',],   
+    ['报告1',],['报告2',],    
     ]
 
 tagdict = {'performance':performance_tag,
@@ -82,7 +89,7 @@ fulllist = getfull( DIRNAME, filelist)
 coursenum = int(input('\n please input a number for 课程: '))    
 coursetype = course_reg.search(filelist[coursenum]).group(1)
 print( fulllist[coursenum])
-print(coursetype)
+logging.critical(coursetype)
 item = {}
 k = 0
 for val in tagdict[coursetype]:
@@ -91,28 +98,26 @@ for val in tagdict[coursetype]:
     k += 1
 itemnum = 6 + int(input('\n please input a number for select item: '))
 
-print( fulllist[coursenum])
-# wb = openpyxl.load_workbook(fulllist[coursenum])
-try:
-#    import pdb;pdb.set_trace()
-    wb = openpyxl.load_workbook(fulllist[coursenum])
-except IndexError:
-    input('Please open the workbook, save it and close it.')
-    wb = openpyxl.load_workbook(fulllist[coursenum])
+wb = openpyxl.load_workbook(fulllist[coursenum])
+
+##try:
+###    import pdb;pdb.set_trace()
+##    wb = openpyxl.load_workbook(fulllist[coursenum])
+##except IndexError:
+##    input('Please open the workbook, save it and close it.')
+##    wb = openpyxl.load_workbook(fulllist[coursenum])
 
 sheet = wb.get_active_sheet()
 
 finish = 0
 while finish != 'y':  
     studnum = input("\n please input three last digitals of select student's number: 205 ")
-    mark = input('\n please input the mark: ')
-    ###
-    
     for row in range(3,sheet.max_row + 1):
         logging.debug(str(sheet['b'+str(row)].value)[-3:])           #  学号在B列
         if str(sheet['b'+str(row)].value)[-3:] == studnum:                   #  学号在B列
             # Write
             logging.critical(sheet.cell(row = row,column = itemnum).value)   # 写之前，cell的值
+            mark = input('\n please input the mark: ')
             sheet.cell(row = row,column = itemnum).value += int(mark)        # 加上要加减的分数
             sheet.cell(row = row,column = 4).value += int(mark)              # 总分也加上该分数
             logging.critical(sheet.cell(row = row,column = itemnum).value)   # 写之后，cell的值
@@ -127,12 +132,15 @@ while finish != 'y':
     for k in marks[:8]:
         print(k)
     logging.critical('---------------')
-    finish = input('你要退出吗？("y" to quit.return to continue):').lower()
-    
-    try:    
-        wb.save(fulllist[coursenum])
-    except PermissionError:
-        print('Please close the workbook.')
+    finish = input('你要退出吗？("y" to quit.Other to continue):').lower()
+
+    while True:
+        try:    
+            wb.save(fulllist[coursenum])
+        except PermissionError:
+            input('Please close the workbook.')
+        else:
+            break
     
 print('后8名为：')
 for k in marks[-8:]:
