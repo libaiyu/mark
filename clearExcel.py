5c4f98cda2b585731890f0dbc5e161af7fffbd13
libaiyu <709097185@qq.com>
2017-05-13 21:49:15 +0800

    modify the notice massage. from getdir import getdir.

--- clearExcel.py
+++ clearExcel.py
@@ -10,32 +10,33 @@ import os
 import re
 import logging
 
-import getdir
+from getdir import getdir
 
 # logging.basicConfig( level = logging.DEBUG, format = ' %(asctime)s - %(levelname)s - %(message)s' )
 logging.basicConfig( level = logging.ERROR, format = ' %(asctime)s - %(levelname)s - %(message)s' )
 logging.critical('--------Start of program---------')
 
-for k in range(3):
-    input("Attention  %d : this will damage all xlsx files' data. Are you sure." % (k + 1))
 
-# find the file that include '学生名单' in filename
-ChineseReg = re.compile(r'学生名单')
-excelReg = re.compile(r'.xlsx')
+do = input("Attention: this will damage all xlsx files' data. y for clear.")
+if do == 'y':
 
-dirname = getdir.getdir()
+    # find the file that include '学生名单' in filename
+    ChineseReg = re.compile(r'学生名单')
+    excelReg = re.compile(r'.xlsx')
 
-wb = openpyxl.Workbook()
-sheet = wb.get_active_sheet()
-count = 0
-for fileName in os.listdir(dirname):
-    if excelReg.search(fileName):
-        if ChineseReg.search(fileName) == None:
-            print('file %s   will be cleared!' % (fileName))
-            input('anykey to continue.')
-            wb.save(dirname + '\\' + fileName)
-            count += 1
-            print('file %s   is clear!' % (fileName))
-print('total %d files is clear!' % (count))
+    dirname = getdir()
+
+    wb = openpyxl.Workbook()
+    sheet = wb.get_active_sheet()
+    count = 0
+    for fileName in os.listdir(dirname):
+        if excelReg.search(fileName):
+            if ChineseReg.search(fileName) == None:
+                print('file %s   will be cleared!' % (fileName))
+                input('anykey to continue.')
+                wb.save(dirname + '\\' + fileName)
+                count += 1
+                print('file %s   is clear!' % (fileName))
+    print('total %d files is clear!' % (count))
 
 logging.critical('-------End--------')
