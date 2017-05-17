02258e85c82d4079482632f75bb2746002deb848
libaiyu <709097185@qq.com>
2017-05-17 20:16:02 +0800

    PE mark can write to a excel file.

--- PE_mark1.py
+++ PE_mark1.py
@@ -6,6 +6,7 @@
 '''
 
 import openpyxl
+
 import logging
 
 
@@ -15,26 +16,21 @@ China_str = ['100米','跳远','铅球','８００米','男子成绩','女子成
 PE_items = ['100米','跳远','铅球','８００米']
 TYPE = ['男子成绩','女子成绩']
 
-measure_v = []
+
+measure_v = {}
 
 def get_params():
-    '''先获取所需的信息：姓名，男女，项目，测量值。
+    '''先获取所需的信息：男女，项目，测量值。
 
 '''
-    wb = openpyxl.load_workbook('高考体育成绩测量表.xlsx')
-    sheet = wb.get_active_sheet()
-    col = 1
-    for row in range(1,sheet.max_row+1):
-        val = sheet.cell( row=row, column=col).value
-        if val:
-            m_val = []
-            for k in range(4):
-                m_val.append( [ PE_items[k], sheet.cell( row=row, column=col+k+2).value])
-            measure_v.append( ( val, sheet.cell( row=row, column=col+1).value, m_val))
-##            print( measure_v)
-##            input('for stop')
-    for t in range(3):
-        print( measure_v[ t])
+
+    # TYPE[0] = '男子成绩' TYPE[1] = '女子成绩'
+    gender = int( input('请选择。\n 0：男子成绩,1：女子成绩--'))
+    # PE_items[0] = '100米', PE_items[1] = '跳远','铅球','８００米'
+    item_name =  int( input('请选择。\n 0：100米,1:跳远,2:铅球,3:８００米--'))
+    # '测得的成绩' 是字典里的键
+    measure_val = float( input( '测得的成绩：'))
+    return gender, item_name, measure_val
 
 def get_mark( gender, item_name, measure_val):
     '''然后根据参数中提供的信息找到相应的成绩。
@@ -45,19 +41,22 @@ def get_mark( gender, item_name, measure_val):
     else:
         mark = mark_man[PE_items[item_name]][measure_val]
     print(mark)
-    return mark
+
 
 # logging.basicConfig( level = logging.DEBUG, format = ' %(asctime)s - %(levelname)s - %(message)s' )
 logging.basicConfig( level = logging.ERROR, format = ' %(asctime)s - %(levelname)s - %(message)s' )
 logging.critical('--------Start of program---------')
 
+
+
 item_head = {}
 mark_man = {}
 mark_woman = {}
+
 wb_val = {}
 
 
-'''读成绩对照表，生成字典的字典。以供后续查询。
+'''读成绩对照表，生成字典。以供后续查询。
 '''
 wb = openpyxl.load_workbook('高考体育成绩对照表.xlsx')
 sheet = wb.get_active_sheet()
@@ -80,7 +79,7 @@ for col, row_val in wb_val.items():
         if val in China_str:
             if val in PE_items:   #  如果值是项目名。
                 item_head[val] = ( col, row)   #  字典--项目名为键，所在的（列，行）为值。
-print(item_head)
+##print(item_head)
 
 for val, ( col, row) in item_head.items():
     key1 = val    #  项目名
@@ -95,49 +94,23 @@ for val, ( col, row) in item_head.items():
         item_woman[key_wo] = val_wo   #  女　的字典
     mark_man[key1] = item_man         #  男　的字典的字典　项目名　为　键，（测量值：成绩）　为　值。
     mark_woman[key1] = item_woman     #  女　的字典的字典　项目名　为　键，（测量值：成绩）　为　值。
-
-##    print('男子成绩\n',mark_man,'\n')
-##    print('女子成绩\n',mark_woman,'\n')
+##print('男子成绩\n',mark_man,'\n')
+##print('女子成绩\n',mark_woman,'\n')
 ##    input('any key, just for debug')
 
-'''读　‘高考体育成绩测量表.xlsx’，生成　字典的字典
-'''
-get_params()
 
-for each in measure_v:
-    print( each)
-    if each[1] == '男':
-        gender = 0
-    elif each[1] == '女':
-        gender = 1
-    for k in range(4):
-        item_name = k
-        measure_val = each[2][k][1]
-        mark = get_mark( gender, item_name, measure_val)
-        each[2][k].append( mark)
 
-wb = openpyxl.Workbook()
-sheet = wb.get_active_sheet()
-row = 0
-for each in measure_v:
-    row += 1
-##    print( each[0])
-    sheet.cell( row = row, column = 1).value = each[0]
-    sheet.cell( row = row, column = 2).value = each[1]
-    sheet.cell( row = row, column = 3).value = each[2][0][0]
-    sheet.cell( row = row, column = 4).value = each[2][0][1]
-    sheet.cell( row = row, column = 5).value = each[2][0][2]
-    sheet.cell( row = row, column = 6).value = each[2][1][0]
-    sheet.cell( row = row, column = 7).value = each[2][1][1]
-    sheet.cell( row = row, column = 8).value = each[2][1][2]
-    sheet.cell( row = row, column = 9).value = each[2][2][0]
-    sheet.cell( row = row, column = 10).value = each[2][2][1]
-    sheet.cell( row = row, column = 11).value = each[2][2][2]    
-    sheet.cell( row = row, column = 12).value = each[2][3][0]
-    sheet.cell( row = row, column = 13).value = each[2][3][1]
-    sheet.cell( row = row, column = 14).value = each[2][3][2]
 
-wb.save('高考体育成绩表.xlsx')
+while True:
+
+    gender, item_name, measure_val = get_params()
+
+    get_mark( gender, item_name, measure_val)
+
+    go_on = input('是否继续查成绩？\n其他:继续，"n":退出。')
+    if go_on == 'n':
+        break
+    
 
 logging.critical('-------End--------')
 
